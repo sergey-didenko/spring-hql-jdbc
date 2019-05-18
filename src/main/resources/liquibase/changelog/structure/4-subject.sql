bcc157a540e664ed0abbc813dc753ae2864ef1e7
Sergey Didenko <sergey.didenko.dev@gmail.com>
2019-05-19 01:30:14 +0300

    fixed sql v2

--- src/main/resources/liquibase/changelog/structure/4-subject.sql
+++ src/main/resources/liquibase/changelog/structure/4-subject.sql
@@ -6,6 +6,6 @@ CREATE TABLE `subject`  (
   `name` varchar(255) DEFAULT NULL,
   `student_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  CONSTRAINT `s_si` FOREIGN KEY (`student_id`) REFERENCES `student` (`id`)
+  CONSTRAINT `s_si` FOREIGN KEY (`student_id`) REFERENCES `student` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
 ) ENGINE = MyISAM AUTO_INCREMENT = 1;
 --rollback drop table subject;
