ae388b8b16ac0ef6e772e7ceff491f5cce69bdc0
Sergey Didenko <sergey.didenko.dev@gmail.com>
2019-05-19 09:20:22 +0300

    upd tests

--- src/test/java/com/sergey/didenko/spring/hqljdbc/MainTests.java
+++ src/test/java/com/sergey/didenko/spring/hqljdbc/MainTests.java
@@ -16,10 +16,7 @@ limitations under the License.
 
 package com.sergey.didenko.spring.hqljdbc;
 
-import com.sergey.didenko.spring.hqljdbc.config.LiquibaseConfiguration;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
