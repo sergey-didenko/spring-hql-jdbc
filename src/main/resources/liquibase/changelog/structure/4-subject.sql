0ada2732ca98dc790e7c7f10850407f8d4df7f4b
Sergey Didenko <sergey.didenko.dev@gmail.com>
2019-05-19 10:29:26 +0300

    fix subject sql

--- src/main/resources/liquibase/changelog/structure/4-subject.sql
+++ src/main/resources/liquibase/changelog/structure/4-subject.sql
@@ -3,7 +3,7 @@
 --changeset Svigelf:4
 CREATE TABLE `subject`  (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
   `student_id` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`),
   CONSTRAINT `s_si` FOREIGN KEY (`student_id`) REFERENCES `student` (`id`) ON DELETE RESTRICT ON UPDATE RESTRICT
